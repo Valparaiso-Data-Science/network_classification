b57185ee8369463a562586d655cd104e231f579f
JamesC78 <jc78@geneseo.edu>
2017-06-26 16:39:36 -0500

    Adjusted the read file lines Fixed typo in plot labels

--- src/models/LassoReg.py
+++ src/models/LassoReg.py
@@ -4,7 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Lasso
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('~/PycharmProjects/network_classification/src/data/data_minmaxscale.csv', index_col=0)
+infile = 'C:/Users/Owner/Documents/VERUM/Network stuff/git/src/data/data_minmaxscale.csv' # --- change this for specific machine
+df = pd.read_csv(infile, index_col=0)
 
 X = df.drop(['Graph', 'Collection'], axis=1).values
 y1 = df['Collection'].values
@@ -21,6 +22,6 @@ index = [0, 1]
 names = np.delete(name, index)
 
 plt.plot(range(len(names)), lasso_coef)
-plt.xticks(range(len(names)), name, rotation=60)
+plt.xticks(range(len(names)), names, rotation=60)
 plt.ylabel = ('Coefficients')
 plt.show()
