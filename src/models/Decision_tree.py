721afbaec853ee2f058657a03f12b734819dd7ad
JamesC78 <jc78@geneseo.edu>
2017-06-26 13:35:08 -0500

    More Data files

--- src/models/Decision_tree.py
+++ src/models/Decision_tree.py
@@ -3,11 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import randint
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
-from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from sklearn.model_selection import RandomizedSearchCV, train_test_split, cross_val_score
 
 
 #df = pd.read_csv('C:/Users/Owner/Documents/VERUM/Network stuff/git/src/data/clean_data.csv', index_col=0)
 df = pd.read_csv('C:/Users/Owner/Documents/VERUM/Network stuff/git/src/data/clean_data_with_new_chem.csv', index_col=0) #Data with down-sampled Cheminfo
+#df = pd.read_csv('C:/Users/Owner/Documents/VERUM/Network stuff/git/src/data/data_minmaxscale.csv', index_col=0) #Data downsampled and scaled
 X = df.drop(['Graph', 'Collection'], axis=1).values
 y = df['Collection'].values
 # Setup the parameters and distributions to sample from: param_dist
@@ -35,8 +36,10 @@ print("Best score is {}".format(tree_cv.best_score_))
 
 tree2 = DecisionTreeClassifier( max_features=7, min_samples_leaf=5)
 
-tree2.fit(X_train, y_train)
+cv_scores = cross_val_score(tree2, X_train, y_train, cv = 5)
+print('average cv score: ', np.mean( cv_scores ))
 
+tree2.fit(X_train, y_train)
 #print( tree2.decision_path(X_train))
 #tree2.decision_path(X_train)
 
