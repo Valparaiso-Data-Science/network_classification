b1082d299903af48bc392f4649a3934d9ef18074
JamesC78 <jc78@geneseo.edu>
2017-06-26 14:50:18 -0500

    Added comments, updated parameters based on results from GridSearchCV

--- src/models/Decision_tree.py
+++ src/models/Decision_tree.py
@@ -7,8 +7,9 @@ from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, train_test
 
 
 #df = pd.read_csv('C:/Users/Owner/Documents/VERUM/Network stuff/git/src/data/clean_data.csv', index_col=0)
-#df = pd.read_csv('C:/Users/Owner/Documents/VERUM/Network stuff/git/src/data/clean_data_with_new_chem.csv', index_col=0) #Data with down-sampled Cheminfo
-df = pd.read_csv('C:/Users/Owner/Documents/VERUM/Network stuff/git/src/data/data_minmaxscale.csv', index_col=0) #Data downsampled and scaled
+#df = pd.read_csv('C:/Users/Owner/Documents/VERUM/Network stuff/git/src/data/clean_data_with_new_chem.csv', index_col=0) #--- Data with down-sampled Cheminfo
+df = pd.read_csv('C:/Users/Owner/Documents/VERUM/Network stuff/git/src/data/data_minmaxscale.csv', index_col=0) #---- Data downsampled and scaled
+
 X = df.drop(['Graph', 'Collection'], axis=1).values
 y = df['Collection'].values
 # Setup the parameters and distributions to sample from: param_dist
@@ -28,27 +29,29 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
 # Instantiate a Decision Tree classifier: tree
 tree = DecisionTreeClassifier()
 
-# Instantiate the RandomizedSearchCV object: tree_cv
+# Instantiate the RandomizedSearchCV object: tree_cv ---- These are commented out as they are only used to determine the best parameters
 #tree_cv = RandomizedSearchCV(tree, param_dist, cv= 5)
-tree_cv = GridSearchCV(tree, param_dist2, cv= 5)
+#tree_cv = GridSearchCV(tree, param_dist2, cv= 5)
 
 # Fit it to the data
-tree_cv.fit(X_train, y_train)
+#tree_cv.fit(X_train, y_train)
 
 # Print the tuned parameters and score
-print("Tuned Decision Tree Parameters: {}".format(tree_cv.best_params_))
-print("Best score is {}".format(tree_cv.best_score_))
+#print("Tuned Decision Tree Parameters: {}".format(tree_cv.best_params_))
+#print("Best score is {}".format(tree_cv.best_score_))
 
 
-tree2 = DecisionTreeClassifier( max_features=7, min_samples_leaf=1)
+tree2 = DecisionTreeClassifier( criterion='entropy', max_depth=24, max_features=8, min_samples_leaf=1)
 
-cv_scores = cross_val_score(tree2, X_train, y_train, cv = 5)
+cv_scores = cross_val_score(tree2, X_train, y_train, cv = 10)
 print('average cv score from training set: ', np.mean( cv_scores ))
 
 tree2.fit(X_train, y_train)
 tree2.predict(X_test)
 test_score = tree2.score(X_test, y_test)
 print('test_score: ', test_score)
+
+
 #print( tree2.decision_path(X_train))
 #tree2.decision_path(X_train)
 
