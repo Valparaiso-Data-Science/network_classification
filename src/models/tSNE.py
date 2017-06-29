e6e98fda74c1e76ae0a6f1c836281ab707982a57
emingram <eingram16@gmail.com>
2017-06-29 11:44:26 -0500

    Add bokeh plot to tsne file

--- src/models/tSNE.py
+++ src/models/tSNE.py
@@ -8,7 +8,9 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler, Imputer
 from sklearn.pipeline import make_pipeline
 import matplotlib.pyplot as plt
-
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import HoverTool, ColumnDataSource
+from bokeh.palettes import d3
 
 
 #net = pd.read_csv('~/PycharmProjects/network_classification/src/data/clean_data_with_new_chem.csv')
@@ -64,3 +66,32 @@ plt.show()
 
 #df = pd.DataFrame({"x" : xs, "y" : ys, "Category Number" : pca['Collection'], "Category Name":collection_names, "Graph Name": graph_names})
 #df.to_csv('~/PycharmProjects/network_classification/src/data/pca_tsne_data.csv')
+
+data = {'x':xs, 'y':ys, 'Collection':collection_names, 'Graph':graph_names}
+df = pd.DataFrame(data)
+
+# Create hover tool
+hover = HoverTool()
+hover.tooltips = [("Graph", "@Graph"),("Category", "@{Collection}")]
+
+# Creating the scatter plot of the x and y coordinates
+p=figure(title = 'PCA t-SNE ', plot_width=1000)
+
+# Color the plot by collection
+category = df['Collection']
+all_categories = category.unique().tolist()
+for i, graph in enumerate(all_categories):
+    source = ColumnDataSource(df[df['Collection'] == graph])
+    p.circle(x='x', y='y', source = source, color = d3['Category20'][16][i], size = 8, legend = graph)
+
+# Creating scatter plot of centroids
+#p.square_cross(centroids_x, centroids_y, color ='black', size = 12, legend = 'Centroid')
+
+# Add tools and interactive legend
+p.add_tools(hover)
+p.legend.location = "top_left"
+p.legend.click_policy="hide"
+
+# Save file and show plot
+output_file('pca_tsne_plot.html')
+show(p)
