8e7b3af15b1b37de1b9056cb600cfc8328b7e920
emingram <eingram16@gmail.com>
2017-06-29 09:55:57 -0500

    Change tSNE to run with PCA data

--- src/models/tSNE.py
+++ src/models/tSNE.py
@@ -1,5 +1,5 @@
 #this program makes a tSNE visualization of data
-#now it imputes, but it will soon not have to do so because james will finish cleaning data up
+#edited to make use of pca data
 #currently using MinMaxScaler and learning rate of 1000.
 
 import pandas as pd
@@ -12,42 +12,52 @@ import matplotlib.pyplot as plt
 
 net = pd.read_csv('~/PycharmProjects/network_classification/src/data/clean_data_with_new_chem.csv')
 
+#data for use with pca file:
+pca = pd.read_csv('~/PycharmProjects/network_classification/src/data/pca_for_tsne.csv')
 #i need a list of the collections as numbers-useful in coloring tsne
-graph_categories = []
-all_categories = net['Collection'].values
 
-all_cat_names = list(net['Collection'].values)
+collection_names = list(pca['Collection'])
+pca['Collection'] = pca['Collection'].astype('category')
+pca['Collection'] = pca['Collection'].cat.codes
+
+#graph_categories = []
+#all_categories = net['Collection'].values
+
+#all_cat_names = list(net['Collection'].values)
 
 
 #creates a list of the existing categories, no repeats
-for category in all_categories:
-    if not category in graph_categories:
-        graph_categories.append( category )
+#for category in all_categories:
+#    if not category in graph_categories:
+#        graph_categories.append( category )
 #re-assigns each collection name in all_categories to an integer, corresponding with its position in graph_categories
-for i in range(len( all_categories )):
-    all_categories[i] = graph_categories.index( all_categories[i] )
+#for i in range(len( all_categories )):
+#    all_categories[i] = graph_categories.index( all_categories[i] )
 
-all_graph_names = list(net['Graph'])
+#all_graph_names = list(net['Graph'])
 
-del net['Graph']
-del net['Collection']
+#del net['Graph']
+#del net['Collection']
 
 
-net_array = net.values
+#net_array = net.values
+pca_array = pca.values
 
-min_max_scale = MinMaxScaler()
+#commented out scaler for pca stuff
+#min_max_scale = MinMaxScaler()
 #imp = Imputer(missing_values='NaN', strategy='most_frequent', axis=0)
+
 tsne = TSNE()
 
-pipeline = make_pipeline(min_max_scale, tsne)
+#pipeline = make_pipeline(min_max_scale, tsne)
+#tsne_features = pipeline.fit_transform(net_array)
 
-
-tsne_features = pipeline.fit_transform(net_array)
+tsne_features = tsne.fit_transform(pca_array)
 xs = tsne_features[:,0]
 ys = tsne_features[:,1]
 
-plt.scatter(xs, ys, c=all_categories)
+plt.scatter(xs, ys, c=pca['Collection'])
 plt.show()
 
-df = pd.DataFrame({"x" : xs, "y" : ys, "Category Number" : all_categories, "Category Name":all_cat_names, "Graph Name": all_graph_names})
-df.to_csv('~/PycharmProjects/network_classification/src/models/new_tsne_data.csv')
+#df = pd.DataFrame({"x" : xs, "y" : ys, "Category Number" : all_categories, "Category Name":all_cat_names, "Graph Name": all_graph_names})
+#df.to_csv('~/PycharmProjects/network_classification/src/models/new_tsne_data.csv')
