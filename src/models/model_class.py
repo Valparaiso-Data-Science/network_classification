2cfa0df55893b87e0a6db75dca5ba16413c311c7
JamesC78 <jc78@geneseo.edu>
2017-06-30 13:48:13 -0500

    Changed train-test split so that it matches 4-fold cv

--- src/models/model_class.py
+++ src/models/model_class.py
@@ -5,6 +5,7 @@ from sklearn.svm import SVC, LinearSVC
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
 
 infile = 'C:/Users/Owner/Documents/VERUM/Network stuff/git/src/data/data_minmaxscale.csv' # -- change for machine
 df = pd.read_csv(infile, index_col=0)
@@ -12,7 +13,7 @@ df = pd.read_csv(infile, index_col=0)
 
 # -- gets rid of small collections -- in the future we will import a file that already has this, so we will delete this block later
 collections = np.unique( df.Collection.values )
-dropoff = 20 #only looks at collections with more than dropoff graphs
+dropoff = 0 #only looks at collections with more than dropoff graphs
 for collection in collections:
     size = len( df[ df.Collection == collection ] )
     if size < dropoff:
@@ -21,13 +22,13 @@ for collection in collections:
 
 X = df.drop(['Graph', 'Collection'], axis=1).values
 y = df['Collection'].values
-split = .3 # -- percent of data to hold for test set
+split = .25 # -- percent of data to hold for test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = split)
 
 
-model = GaussianNB()  # -- change this for the model you want to use
+model = DecisionTreeClassifier()  # -- change this for the model you want to use
 
-k = 5
+k = 4
 cvscores = cross_val_score(model, X, y, cv = k)
 print(k, '-fold cross validation')
 print('cv scores: ', cvscores)
