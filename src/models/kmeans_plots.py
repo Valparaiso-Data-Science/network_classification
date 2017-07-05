65eb8fb66124c2c43250af061c3c4ef6bfd38cee
emingram <eingram16@gmail.com>
2017-07-05 14:27:28 -0500

    Create basic kmeans script

---
+++ src/models/kmeans_plots.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+net = pd.read_csv('~/PycharmProjects/network_classification/src/data/data_minmaxscale.csv')
+
+collection = list(net['Collection'])
+graph = list(net['Graph'])
+
+del net['Graph']
+del net['Collection']
+net_array = net.values
+
+kmeans = KMeans(n_clusters=8)
+labels = kmeans.fit_predict(net_array)
+
+#***************
+# TESTING TO SEE WHAT IS HAPPENING
+#***************
+#print(labels)
+#net_ct = pd.DataFrame({'Labels':labels, 'Collection':collection})
+#ct = pd.crosstab( net_ct['Collection'], net_ct['Labels'])
+#print(ct)
