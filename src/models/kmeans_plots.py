ddf1dfe52eb4688dffd79540318078fb61be5136
emingram <eingram16@gmail.com>
2017-07-05 15:21:15 -0500

    Create basic boxplots for label 0

--- src/models/kmeans_plots.py
+++ src/models/kmeans_plots.py
@@ -10,7 +10,7 @@ graph = list(net['Graph'])
 del net['Graph']
 del net['Collection']
 net_array = net.values
-
+columns = net.columns
 kmeans = KMeans(n_clusters=8)
 labels = kmeans.fit_predict(net_array)
 
@@ -36,4 +36,24 @@ c4 = net[net['Label']==4]
 c5 = net[net['Label']==5]
 c6 = net[net['Label']==6]
 c7 = net[net['Label']==7]
+
+#***************
+# GENERATING PLOTS
+#***************
+
+#let's start with just one label:0
+
+#column = c0.iloc[:,0].values
+#plt.boxplot(column)
+#plt.show()
+
+#names = {v:k for v, k in enumerate(columns)}
+i=0
+#attempting a for loop
+for name in columns:
+    plt.boxplot(c0.iloc[:,i].values)
+    i = i+1
+    plt.title('Label 0 ' + str(name))
+    plt.show()
+
 
