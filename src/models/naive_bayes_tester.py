6bb05f574c30e5b442a8567c18a316b3c85e6f11
JamesC78 <jc78@geneseo.edu>
2017-07-10 16:47:14 -0500

    Added beginning of function, not working yet

--- src/models/naive_bayes_tester.py
+++ src/models/naive_bayes_tester.py
@@ -9,3 +9,29 @@ from git.src.models.model_class import modelFitTest
 
 infile = 'C:/Users/Owner/Documents/VERUM/Network stuff/git/src/data/data_minmaxscale.csv' # -- change for machine
 df = pd.read_csv(infile, index_col=0)
+
+maxTri_assort_minDeg_scores = [] # sets up list for all cv score averages
+iterations = 20
+toDrop = ['Graph', 'Collection',
+          # 'Nodes',
+          # 'Edges',
+          # 'Density',
+          # 'Maximum degree',
+           'Minimum degree',
+          # 'Average degree',
+           'Assortativity',
+          # 'Total triangles',
+          # 'Average triangles',
+           'Maximum triangles',
+          # 'Avg. clustering coef.',
+          # 'Frac. closed triangles',
+          # 'Maximum k-core',
+          # 'Max. clique (lb)'
+            ]
+#for i in range(iterations):
+#    score = modelFitTest(GaussianNB, df, dropList=toDrop, cv=5, feat_comp=True)
+#    maxTri_assort_minDeg_scores.append(score)
+#
+#print(maxTri_assort_minDeg_scores)
+
+print(modelFitTest(GaussianNB, df, feat_comp=True))
