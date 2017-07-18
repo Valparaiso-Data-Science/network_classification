7e2cc7bf75c010ed916804d29061de66fa4d8cf5
adriananaortiz <adriana.ortiz10@upr.edu>
2017-07-18 13:10:20 -0500

    Added file that retrieves the DIMACS and BHOSLIB collections only

--- src/data/Get_dimacs_and_bhoslib.py
+++ src/data/Get_dimacs_and_bhoslib.py
@@ -1,4 +1,4 @@
-# This program retrieves the miscellaneous collection
+# This program retrieves the collections DIMACS, DIMACS10 and BHOSLIB
 # from the raw file 'network-meta-statistics2.csv'
 
 import pandas as pd
@@ -12,14 +12,20 @@ df.columns = (['Graph', 'Collection', 'Nodes', 'Edges', 'Density', 'Maximum degr
        'Average triangles', 'Maximum triangles', 'Avg. clustering coef.',
         'Frac. closed triangles', 'Maximum k-core', 'Max. clique (lb)', 'Chromatic Number'])
 
-# Get only the miscellaneous data
-misc_data = df[df['Collection'] == 'Miscellaneous Networks']
+# Get only the dimacs and bhoslib collections
+dimacs_data = df[df['Collection'] == 'DIMACS']
+dimacs10_data = df[df['Collection'] == 'DIMACS10']
+bhoslib_data = df[df['Collection'] == 'BHOSLIB']
+
+df_new = pd.concat([dimacs_data, dimacs10_data, bhoslib_data])
 
 # Get rid of Chromatic Number
-del misc_data['Chromatic Number']
+del df_new['Chromatic Number']
 
 # Drop NaN values of the frame
-misc_data = misc_data.dropna()
+# Doing this drops 7 graphs
+df_new = df_new.dropna()
+
 
 # Move data to a csv file
-misc_data.to_csv('~/Downloads/network_classification/src/data/miscellaneous_networks.csv')
+df_new.to_csv('~/Downloads/network_classification/src/data/dimacs_and_bhoslib_networks.csv')
