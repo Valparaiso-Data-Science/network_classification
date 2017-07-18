06e53e24d786ea8d08347c14953bea5adf23b508
emingram <eingram16@gmail.com>
2017-07-18 09:14:07 -0500

    New p and m values, comment out writing lines

--- src/data/random_gen.py
+++ src/data/random_gen.py
@@ -4,7 +4,7 @@ import numpy as np
 er = Graph()
 b = Graph()
 
-p_list_1 = [0.05, 0.1, 0.2]
+p_list_1 = [0.05, 0.1, 0.]
 p_list_2 = [0.0005, 0.005, 0.001]
 p_list_3 = [0.000005, 0.00005, 0.0005]
 m_list_1 = [10, 40, 60]
@@ -21,12 +21,12 @@ for n1 in nGraphs:
     p_str = str(int(p * 100))
     er = er.Erdos_Renyi(n_1, p)
     #write er to file
-    er.write_edgelist('er_1_'+str(n1)+ '_'+p_str+'.csv')
+    #er.write_edgelist('er_1_'+str(n1)+ '_'+p_str+'.csv')
     m = int(np.random.choice(m_list_1))
     b = b.Barabasi(n_1, m)
     m_str = str(m)
     #write b to file
-    b.write_edgelist('b_1_'+str(n1)+ '_'+m_str+'.csv')
+    #b.write_edgelist('b_1_'+str(n1)+ '_'+m_str+'.csv')
     if n1 == 13:
         print('halfway done with n1!')
 print('done with n1!')
@@ -35,15 +35,15 @@ print('working on n2! no sweat!')
 for n2 in nGraphs:
     n_2 = 10000 + np.random.randint(-2000, 2000)
     p = np.random.choice(p_list_2)
-    p_str = str(int(p * 100))
+    p_str = str(int(p * 10000))
     er = er.Erdos_Renyi(n_2, p=p)
     #write er to file
-    er.write_edgelist('er_2_'+str(n2)+ '_'+p_str+'.csv')
+    #er.write_edgelist('er_2_'+str(n2)+ '_'+p_str+'.csv')
     m = int(np.random.choice(m_list_2))
     b = b.Barabasi(n_2, m)
     m_str = str(m)
     # write b to file
-    b.write_edgelist('b_2_'+str(n2)+'_'+m_str+'.csv')
+    #b.write_edgelist('b_2_'+str(n2)+'_'+m_str+'.csv')
     if n2 == 13:
         print('halfway done with n2!')
 print('relax! done with n2!!')
@@ -51,10 +51,8 @@ print('relax! done with n2!!')
 for n3 in nGraphs:
     print('working on graph '+str(n3) + '!!')
     n_3 = 100000 + np.random.randint(-20000, 20000)
-    p = np.random.choice(p_list_3)
-    if p == 0.1:
-        print('be patient, im really big!'
-    p_str = str(int(p * 100))
+    p = np.random.choice(p_list_3)#
+    p_str = str(int(p * 1000000))
     er = er.Erdos_Renyi(n_3, p=p)
     #write er to file
     er.write_edgelist('er_3_'+str(n3)+ '_'+p_str+'.csv')
@@ -62,7 +60,7 @@ for n3 in nGraphs:
     b = b.Barabasi(n_3, m)
     m_str = str(m)
     # write b to file
-    b.write_edgelist('b_3_'+str(n3)+'_'+m_str+'.csv')
+    #b.write_edgelist('b_3_'+str(n3)+'_'+m_str+'.csv')
     if n3 == 13:
         print('halfway done with n3! staying alive!')
 print('done!!')
