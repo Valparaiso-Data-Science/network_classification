91d025f5fbb98d942e6cf3609545f17c373875b1
emingram <eingram16@gmail.com>
2017-07-27 15:11:29 -0500

    Add function to make inertia plot

--- src/data/Clean_Chem.py
+++ src/data/Clean_Chem.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
 
-
-net = pd.read_csv('~/PycharmProjects/network_classification/data/interim/data_without_dimacs_or_bhoslib.csv', index_col = 0)
+net = pd.read_csv('~/PycharmProjects/network_classification/src/data/data_without_dimacs_or_bhoslib.csv', index_col = 0)
 
 chem = net[net['Collection'] == 'Cheminformatics']
 
@@ -17,6 +17,34 @@ del chem['Chromatic Number']
 chem_array = chem_kmeans.values
 scale = MinMaxScaler()
 
+#**************************
+# Getting inertia graph
+#**************************
+def inertia_plot(data_array):
+    ks = range(1, 20)
+    inertias = []
+
+    for k in ks:
+        # Create a KMeans instance with k clusters: model
+        model = KMeans(n_clusters=k)
+
+        # Fit model to samples
+        # Change to the data we need to check
+        model.fit(data_array)
+
+        # Append the inertia to the list of inertias
+        inertias.append(model.inertia_)
+
+    # Plot ks vs inertias
+    plt.plot(ks, inertias, '-o')
+    plt.xlabel('number of clusters, k')
+    plt.ylabel('inertia')
+    plt.xticks(ks)
+    plt.title('Cheminformatics Inertia vs. k')
+    plt.show()
+
+
+inertia_plot(chem_array)
 kmeans = KMeans(n_clusters=4)
 
 kmeans.fit(chem_array)
