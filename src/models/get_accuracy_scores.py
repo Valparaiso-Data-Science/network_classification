Fixed bug in train_predict method with droplist and scaling ran more tests

src/models/get_accuracy_scores.py
@@ -17,15 +17,15 @@ df = pd.read_csv(infile, index_col=0)
 
 remove = ['Graph', 'Collection',
           # 'Nodes',
-          # 'Edges',
+           'Edges',
           # 'Density',
            'Maximum degree',
            'Minimum degree',
           # 'Average degree',
-           'Assortativity',
+          # 'Assortativity',
             'Total triangles',
-          # 'Average triangles',
-          # 'Maximum triangles',
+           'Average triangles',
+           'Maximum triangles',
           # 'Avg. clustering coef.',
           # 'Frac. closed triangles',
           # 'Maximum k-core',
@@ -34,6 +34,46 @@ remove = ['Graph', 'Collection',
 tester = ModelTester(df)
 forestModel = RandomForestClassifier(30)
 NBModel = GaussianNB()
+
+
+
+
+
+#***********************************************
+# For getting accuracy of combined collections
+#***********************************************
+
+combined = tester.combine_collections(['Brain Networks', 'Biological Networks'], 'Brain-Bio')
+combTesterBnBio = ModelTester(combined)
+
+combined = tester.combine_collections(['Web Graphs', 'Technological Networks'], 'Web-Tech')
+combTesterWbTch = ModelTester(combined)
+
+combined = combTesterBnBio.combine_collections(['Web Graphs', 'Technological Networks'], 'Web-Tech')
+combTesterBoth = ModelTester(combined)
+
+scoresArr = np.zeros(3)
+for i in range(100):
+    webTech = combTesterWbTch.modelFitTest(NBModel, dropList=remove, prnt=False, LOO=True)
+    brainBio = combTesterBnBio.modelFitTest(NBModel, dropList=remove, prnt=False, LOO=True)
+    both = combTesterBoth.modelFitTest(NBModel, dropList=remove, prnt=False, LOO=True)
+
+    currentScores = [ webTech,
+                      brainBio,
+                      both
+                      ]
+    arr = np.array(currentScores)
+    scoresArr += arr
+    print( 'Iteration ', i )
+
+
+scoresAv = scoresArr / 100
+print('WebTech: ', scoresAv[0])
+print('BrainBio: ', scoresAv[1])
+print('Both: ', scoresAv[2])
+
+sys.exit('Im done')
+
 
 
 #*************************************************************
@@ -86,41 +126,8 @@ print('Web: ', averageDT[5])
 
 end = time.time()
 print('time: ', end-start)
-sys.exit("I'm done")
-#***********************************************
-# For getting accuracy of combined collections
-#***********************************************
-
-combined = tester.combine_collections(['Brain Networks', 'Biological Networks'], 'Brain-Bio')
-combTesterBnBio = ModelTester(combined)
-
-combined = tester.combine_collections(['Web Graphs', 'Technological Networks'], 'Web-Tech')
-combTesterWbTch = ModelTester(combined)
-
-combined = combTesterBnBio.combine_collections(['Web Graphs', 'Technological Networks'], 'Web-Tech')
-combTesterBoth = ModelTester(combined)
-
-scoresArr = np.zeros(3)
-for i in range(100):
-    webTech = combTesterWbTch.modelFitTest(forestModel, prnt=False, LOO=True)
-    brainBio = combTesterBnBio.modelFitTest(forestModel, prnt=False, LOO=True)
-    both = combTesterBoth.modelFitTest(forestModel, prnt=False, LOO=True)
-
-    currentScores = [ webTech,
-                      brainBio,
-                      both
-                      ]
-    arr = np.array(currentScores)
-    scoresArr += arr
-    print( 'Iteration ', i )
 
 
-scoresAv = scoresArr / 100
-print('WebTech: ', scoresAv[0])
-print('BrainBio: ', scoresAv[1])
-print('Both: ', scoresAv[2])
-
-sys.exit('Im done')
 #start = time.time()
 
 

src/models/model_class.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold, cross_val_predict, LeaveOneOut
 from sklearn.metrics import classification_report, confusion_matrix, f1_score
 from sklearn.preprocessing import MinMaxScaler
+import sys
 
 
 #infile = 'C:/Users/Owner/Documents/VERUM/Network stuff/git/src/data/data_minmaxscale.csv' # -- change for machine
@@ -319,8 +320,9 @@ class ModelTester():
         dfNew = self.df.copy()
         graphs = list(dfNew['Graph'])
         categories = list(dfNew['Collection'])
-        del dfNew['Graph']
-        del dfNew['Collection']
+        dfNew = dfNew.drop(dropList, axis=1)
+        #del dfNew['Graph']
+        #del dfNew['Collection']
 
         array = dfNew.values
         scale = MinMaxScaler()
@@ -333,16 +335,17 @@ class ModelTester():
         cols = list(scaleDF.columns)
         cols = cols[-2:] + cols[:-2]
         scaleDF = scaleDF[cols]
-
+       # print(scaleDF.head())
         # This removes the collections that are too small
         collections = np.unique(scaleDF.Collection.values)
         for collection in collections:
             size = len(scaleDF[scaleDF.Collection == collection])
             if size < minSize:
                 scaleDF = scaleDF[scaleDF.Collection != collection]
+       # print(scaleDF.info())
 
 # Creates the training data
-        X =scaleDF.drop(dropList, axis=1).values
+        X =scaleDF.drop(['Graph', 'Collection'], axis=1).values
         y = scaleDF['Collection'].values
 
 # Sets up the testing data

src/models/model_test.py
@@ -9,6 +9,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+import sys
 
 #from git.src.models.model_class import modelFitTest
 from git.src.models.model_class import ModelTester
@@ -85,14 +86,15 @@ renamedMisc = miscObject.combine_collections(['Brain Networks', 'Biological Netw
 #miscComb = combTester.train_predict(forestModel, renamedMisc)
 
 miscScore = []
-#for i in range(100):
-#    miscTest = combTester.train_predict(forestModel, renamedMisc, minSize=16)
-#    correct = len(miscTest[miscTest.Hypothesis == miscTest.Predicted].Name.values)
-#    score = correct / 50
-#    miscScore.append(score)
+for i in range(100):
+    miscTest = combTester.train_predict(GNBmodel, renamedMisc, dropList=remove)
+    correct = len(miscTest[miscTest.Hypothesis == miscTest.Predicted].Name.values)
+    score = correct / 50
+    miscScore.append(score)
 
-#print(miscScore)
-#print('average score: ', np.mean(miscScore))
+print(miscTest[miscTest.Hypothesis == miscTest.Predicted])
+print(miscScore)
+print('average score: ', np.mean(miscScore))
 
 #miscTest = combTester.train_predict(forestModel, renamedMisc, minSize=16)
 #print(miscTest)
@@ -101,7 +103,7 @@ miscScore = []
 #miscAnalysis = combTester.get_mislabeled_graphs(forestModel, externalData=renamedMisc, minSize=16)
 #print(miscAnalysis)
 
-
+sys.exit("I'm done'")
 
 infile_er = 'C:/Users/Owner/Documents/VERUM/Network stuff/git/src/data/synthetic_e1e2e3_complete.csv'
 df_er = pd.read_csv(infile_er, index_col=0)