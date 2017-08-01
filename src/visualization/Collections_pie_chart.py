11ffca5a4243e0915af45640ee87035b040121d3
Karl R. B. Schmitt <karl.schmitt@valpo.edu>
2017-08-01 10:14:30 -0500

    Modified bar plots of category counts

--- src/visualization/Collections_pie_chart.py
+++ src/visualization/Collections_pie_chart.py
@@ -7,8 +7,8 @@ from bokeh.charts import Bar, output_file, show
 
 
 # Read file
-df = pd.read_csv('~/Downloads/network_classification/src/data/data_minmaxscale.csv', index_col=0)
-df_old = pd.read_csv('~/Downloads/network_classification/src/data/data_without_dimacs_or_bhoslib.csv', index_col=0)
+df = pd.read_csv('./../data/data_minmaxscale.csv', index_col=0)
+df_old = pd.read_csv('./../data/data_without_dimacs_or_bhoslib.csv', index_col=0)
 
 # Plot pie chart with Collection column of df
 #df['Collection'].value_counts().plot.pie(fontsize = 10.5, autopct='%1.0f%%')
@@ -17,14 +17,23 @@ df_old = pd.read_csv('~/Downloads/network_classification/src/data/data_without_d
 ## Show plot
 #plt.show()
 my_colors = 'rgbkymc'
-df_old['Collection'].value_counts().plot.bar(color=my_colors, fontsize=15)
-plt.title('Counts of Collections Before Downsampling', fontsize=25)
-#plt.show()
+p1 = plt.figure()
+myfig=df_old['Collection'].value_counts().plot.bar(color=my_colors, fontsize=15)
+
+#plt.title('Counts of Collections Before Downsampling', fontsize=25)
+myfig.set_axisbelow(True)
+myfig.yaxis.grid(color='gray', linestyle='dashed')
 
 
 ##plot bar chart with collection column of df
-df['Collection'].value_counts().plot.bar(color=my_colors, fontsize=15)
-plt.title('Counts of Collections After Downsampling', fontsize=25)
+p2 = plt.figure()
+myfig2 = df['Collection'].value_counts().plot.bar(color=my_colors, fontsize=15)
+#plt.title('Counts of Collections After Downsampling', fontsize=25)
+myfig2.set_axisbelow(True)
+myfig2.yaxis.grid(color='gray', linestyle='dashed')
+
+show(p1,p2)
+#myfig2.show()
 
 # Show plot
 #plt.show()
@@ -38,14 +47,14 @@ plt.title('Counts of Collections After Downsampling', fontsize=25)
 #p.vbar(bottom=0, top=df['Collection'].value_counts(), x=df['Collection'].unique(), width=100)
 #p.vbar(bottom=0, top=df_old['Collection'].value_counts(), x=df_old['Collection'].unique(), width=100)
 
-amounts = [119,114,62,48,36,22,19,16,13,11,9,9,8,6,2]
-names = ['Cheminformatics', 'Facebook Networks', 'Retweet Networks', 'Social Networks', 'Brain Networks',
-        'Web Graphs', 'Interaction Networks', 'Collaboration Networks', 'Recommendation Networks',
-         'Scientific Computing', 'Technological Networks', 'Biological Networks', 'Infrastructure Networks',
-         'Ecology Networks', 'Massive Network Data']
+#amounts = [119,114,62,48,36,22,19,16,13,11,9,9,8,6,2]
+#names = ['Cheminformatics', 'Facebook Networks', 'Retweet Networks', 'Social Networks', 'Brain Networks',
+#        'Web Graphs', 'Interaction Networks', 'Collaboration Networks', 'Recommendation Networks',
+#         'Scientific Computing', 'Technological Networks', 'Biological Networks', 'Infrastructure Networks',
+#         'Ecology Networks', 'Massive Network Data']
 
-source = ColumnDataSource(dict(x=amounts, y = names))
+#source = ColumnDataSource(dict(x=amounts, y = names))
 
-p = Bar(df, values=amounts, label='Collection',group='Collection', title="Total MPG by CYL")
+#p = Bar(df, values=amounts, label='Collection',group='Collection', title="Total MPG by CYL")
 
-show(p)
+#show(p)
