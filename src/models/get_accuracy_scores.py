a6de952e89911940f4e3ceea6857ee8328355e17
JamesC78 <jc78@geneseo.edu>
2017-08-02 01:23:24 -0500

    tested misc networks with comb collections

--- src/models/get_accuracy_scores.py
+++ src/models/get_accuracy_scores.py
@@ -35,6 +35,211 @@ tester = ModelTester(df)
 forestModel = RandomForestClassifier(30)
 NBModel = GaussianNB()
 
+#***********************************************
+# For getting accuracy of Miscellaneous
+#***********************************************
+
+# Starts by combining collections in df
+combined = tester.combine_collections(['Brain Networks', 'Biological Networks'], 'Brain-Bio')
+combTester = ModelTester(combined)
+combined = combTester.combine_collections(['Web Graphs', 'Technological Networks'], 'Web-Tech')
+combTester = ModelTester(combined)
+combined = combTester.combine_collections(['Ecology Networks', 'Scientific Computing'], 'Sci-Eco')
+combTester = ModelTester(combined)
+
+# Makes the same combinations for the misc df
+miscFile = 'C:/Users/Owner/Documents/VERUM/Network stuff/git/src/data/reduced_miscellaneous_networks.csv'
+misc = pd.read_csv(miscFile, index_col=0)
+miscObject = ModelTester(misc)
+
+renamedMisc = miscObject.combine_collections(['Web Graphs', 'Technological Networks'], 'Web-Tech')
+miscObject = ModelTester(renamedMisc)
+renamedMisc = miscObject.combine_collections(['Brain Networks', 'Biological Networks'], 'Brain-Bio')
+miscObject = ModelTester(renamedMisc)
+renamedMisc = miscObject.combine_collections(['Ecology Networks', 'Scientific Computing'], 'Sci-Eco')
+
+
+miscScore = []
+iterations = 500
+for i in range(iterations):
+    print('iteration: ', i)
+    miscTest = combTester.train_predict(forestModel, renamedMisc)
+    correct = len(miscTest[miscTest.Hypothesis == miscTest.Predicted].Name.values)
+    score = correct / 50
+    miscScore.append(score)
+#
+#print(miscTest[miscTest.Hypothesis == miscTest.Predicted])
+#print(miscScore)
+print('average score: ', np.mean(miscScore))
+
+#miscTest = combTester.train_predict(forestModel, renamedMisc, minSize=16)
+#print(miscTest)
+#print(miscTest.info())
+#print(miscTest[ miscTest.Hypothesis == miscTest.Predicted])
+#miscAnalysis = combTester.get_mislabeled_graphs(forestModel, externalData=renamedMisc, minSize=16)
+#print(miscAnalysis)
+
+
+sys.exit()
+
+#***********************************************
+# For getting accuracy of combined collections
+#***********************************************
+
+df = df[ df.Collection != 'Interaction Networks' ]
+df = df[ df.Collection != 'Collaboration Networks' ]
+tester = ModelTester(df)
+
+combined = tester.combine_collections(['Brain Networks', 'Biological Networks'], 'Brain-Bio')
+combTesterBnBio = ModelTester(combined)
+
+combined = tester.combine_collections(['Web Graphs', 'Technological Networks'], 'Web-Tech')
+combTesterWbTch = ModelTester(combined)
+
+combined = tester.combine_collections(['Scientific Computing', 'Ecology Networks'], 'Sci-Eco')
+combTesterSciEco = ModelTester(combined)
+
+combined = combTesterBnBio.combine_collections(['Web Graphs', 'Technological Networks'], 'Web-Tech')
+combTesterBB_WT = ModelTester(combined)
+
+combined = combTesterBnBio.combine_collections(['Scientific Computing', 'Ecology Networks'], 'Sci-Eco')
+combTesterBB_SE = ModelTester(combined)
+
+combined = combTesterWbTch.combine_collections(['Scientific Computing', 'Ecology Networks'], 'Sci-Eco')
+combTesterWT_SE = ModelTester(combined)
+
+combined = combTesterWT_SE.combine_collections(['Brain Networks', 'Biological Networks'], 'Brain-Bio')
+combTesterBB_WT_SE = ModelTester(combined)
+
+numScores = 4
+scoresArr = np.zeros(numScores)
+iterations = 100
+for i in range(iterations):
+    print( 'Iteration ', i )
+    #webTech = combTesterWbTch.modelFitTest(forestModel,prnt=False, LOO=True)
+    #brainBio = combTesterBnBio.modelFitTest(forestModel, prnt=False, LOO=True)
+    sciEco = combTesterSciEco.modelFitTest(forestModel, prnt=False, LOO=True, minSize=16)
+    #BB_WT = combTesterBB_WT.modelFitTest(forestModel, prnt=False, LOO=True)
+    BB_SE = combTesterBB_SE.modelFitTest(forestModel, prnt=False, LOO=True, minSize=16)
+    WT_SE = combTesterWT_SE.modelFitTest(forestModel, prnt=False, LOO=True, minSize=16)
+    BB_WT_SE = combTesterBB_WT_SE.modelFitTest(forestModel, prnt=False, LOO=True, minSize=16)
+
+    currentScores = [ #webTech,
+                      #brainBio,
+                      sciEco,
+                      #BB_WT,
+                      BB_SE,
+                      WT_SE,
+                      BB_WT_SE
+                      ]
+    arr = np.array(currentScores)
+    scoresArr += arr
+
+
+scoresAv = scoresArr / iterations
+#print('WebTech: ', scoresAv[0])
+#print('BrainBio: ', scoresAv[1])
+print('sciEco: ', scoresAv[0])
+#print('Brain-Bio Web-Tech: ', scoresAv[3])
+print('Brain-Bio Sci-Eco: ', scoresAv[1])
+print('Web-Tech Sci-Eco: ', scoresAv[2])
+print('Brain-Bio Web-Tech Sci-Eco: ', scoresAv[3])
+
+sys.exit('Im done')
+
+#**************************************************************
+# For getting accuracy of 6 models
+#**************************************************************
+start = time.time()
+
+
+# This top section is for including synthetic graphs
+# Erdos Renyi
+infile_er = 'C:/Users/Owner/Documents/VERUM/Network stuff/git/src/data/synthetic_e1e2e3_complete.csv'
+df_er = pd.read_csv(infile_er)
+for i in df_er.index:
+    df_er.Collection.iloc[i] = 'Synthetic Erdos Renyi'
+# Barabasi
+infile_b1b2 = 'C:/Users/Owner/Documents/VERUM/Network stuff/synthetic_features_b1b2.csv'
+df_b1b2 = pd.read_csv(infile_b1b2)
+for i in df_b1b2.index:
+    df_b1b2.Collection.iloc[i] = 'Synthetic Barabasi'
+
+dfSyn = pd.concat([df, df_er, df_b1b2])
+tester = ModelTester(dfSyn)
+
+# Finds both average accuracy and std dev of scores
+numScores = 4 # -- this should be changed to match the number of scores generated each iteration
+scoresArr = np.array([[0]* numScores ])
+
+for i in range(100):
+    print('iteration ', i)
+    RF = tester.modelFitTest(forestModel, prnt=False)
+    RFLOO = tester.modelFitTest(forestModel, LOO=True, prnt=False)
+    NB = tester.modelFitTest(NBModel, dropList=remove, prnt=False)
+    NBLOO = tester.modelFitTest(NBModel, LOO=True, dropList=remove, prnt=False)
+    #DT = tester.modelFitTest(DecisionTreeClassifier(), prnt=False)
+    #DTLOO = tester.modelFitTest(DecisionTreeClassifier(), LOO=True, prnt=False)
+    #Lin = tester.modelFitTest(LinearSVC(), prnt=False)
+    #LinLOO = tester.modelFitTest(LinearSVC(), LOO=True, prnt=False)
+    #svc = tester.modelFitTest(SVC(), prnt=False)
+    #svcLOO = tester.modelFitTest(SVC(), LOO=True, prnt=False)
+    #Log = tester.modelFitTest(LogisticRegression(), prnt=False)
+    #LogLOO = tester.modelFitTest(LogisticRegression(), LOO=True, prnt=False)
+
+    currentScores = [RF, RFLOO,
+                 NB, NBLOO,
+                # DT, DTLOO,
+                # Lin, LinLOO,
+                # svc, svcLOO,
+                # #Log, LogLOO
+                 ]
+    arr = np.array([currentScores])
+    scoresArr = np.concatenate((scoresArr, arr), axis=0)
+    #print(scoresArr)
+    #scoresArr += currentScores
+
+    if i == 49:
+        mid = time.time()
+        print('half way time: ', mid-start)
+#scoresAv = scoresArr / 100
+
+scoresArr = np.delete(scoresArr, 0, 0)
+scoresStdDev = np.std(scoresArr, axis=0)
+scoresAv = np.mean(scoresArr, axis=0)
+
+
+print('RF: ', scoresStdDev[0])
+print('RFLOO: ', scoresStdDev[1])
+print('NB: ', scoresStdDev[2])
+print('NBLOO: ', scoresStdDev[3])
+#print('DT: ', scoresStdDev[4])
+#print('DTLOO: ', scoresStdDev[5])
+#print('Lin: ', scoresStdDev[6])
+#print('LinLOO: ', scoresStdDev[7])
+#print('svc: ', scoresStdDev[8])
+#print('svcLOO: ', scoresStdDev[9])
+#print('Log: ', scoresStdDev[10])
+#print('LogLOO: ', scoresStdDev[11])
+
+print('RF: ', scoresAv[0])
+print('RFLOO: ', scoresAv[1])
+print('NB: ', scoresAv[2])
+print('NBLOO: ', scoresAv[3])
+#print('DT: ', scoresAv[4])
+#print('DTLOO: ', scoresAv[5])
+#print('Lin: ', scoresAv[6])
+#print('LinLOO: ', scoresAv[7])
+#print('svc: ', scoresAv[8])
+#print('svcLOO: ', scoresAv[9])
+#print('Log: ', scoresAv[10])
+#print('LogLOO: ', scoresAv[11])
+
+
+end = time.time()
+print('total time: ', end-start)
+
+
 
 #*************************************************************
 # For getting f1 score among collections for different models
@@ -100,93 +305,6 @@ print('time: ', end-start)
 
 sys.exit("I'm done")
 
-#**************************************************************
-# For getting accuracy of 6 models
-#**************************************************************
-start = time.time()
-
-
-# This top section is for including synthetic graphs
-# Erdos Renyi
-infile_er = 'C:/Users/Owner/Documents/VERUM/Network stuff/git/src/data/synthetic_e1e2e3_complete.csv'
-df_er = pd.read_csv(infile_er)
-for i in df_er.index:
-    df_er.Collection.iloc[i] = 'Synthetic Erdos Renyi'
-# Barabasi
-infile_b1b2 = 'C:/Users/Owner/Documents/VERUM/Network stuff/synthetic_features_b1b2.csv'
-df_b1b2 = pd.read_csv(infile_b1b2)
-for i in df_b1b2.index:
-    df_b1b2.Collection.iloc[i] = 'Synthetic Barabasi'
-
-# Currently changed to find std. dev.
-scoresArr = np.array([[0]*12])
-for i in range(100):
-    print('iteration ', i)
-    RF = tester.modelFitTest(forestModel, prnt=False)
-    RFLOO = tester.modelFitTest(forestModel, LOO=True, prnt=False)
-    NB = tester.modelFitTest(NBModel, dropList=remove, prnt=False)
-    NBLOO = tester.modelFitTest(NBModel, LOO=True, dropList=remove, prnt=False)
-    DT = tester.modelFitTest(DecisionTreeClassifier(), prnt=False)
-    DTLOO = tester.modelFitTest(DecisionTreeClassifier(), LOO=True, prnt=False)
-    Lin = tester.modelFitTest(LinearSVC(), prnt=False)
-    LinLOO = tester.modelFitTest(LinearSVC(), LOO=True, prnt=False)
-    svc = tester.modelFitTest(SVC(), prnt=False)
-    svcLOO = tester.modelFitTest(SVC(), LOO=True, prnt=False)
-    Log = tester.modelFitTest(LogisticRegression(), prnt=False)
-    LogLOO = tester.modelFitTest(LogisticRegression(), LOO=True, prnt=False)
-
-    currentScores = [RF, RFLOO,
-                 NB, NBLOO,
-                 DT, DTLOO,
-                 Lin, LinLOO,
-                 svc, svcLOO,
-                 Log, LogLOO
-                 ]
-    arr = np.array([currentScores])
-    scoresArr = np.concatenate((scoresArr, arr), axis=0)
-    #print(scoresArr)
-    #scoresArr += currentScores
-
-    if i == 49:
-        mid = time.time()
-        print('half way time: ', mid-start)
-#scoresAv = scoresArr / 100
-
-#print(scoresAv)
-scoresArr = np.delete(scoresArr, 0, 0)
-scoresStdDev = np.std(scoresArr, axis=0)
-
-print('RF: ', scoresStdDev[0])
-print('RFLOO: ', scoresStdDev[1])
-print('NB: ', scoresStdDev[2])
-print('NBLOO: ', scoresStdDev[3])
-print('DT: ', scoresStdDev[4])
-print('DTLOO: ', scoresStdDev[5])
-print('Lin: ', scoresStdDev[6])
-print('LinLOO: ', scoresStdDev[7])
-print('svc: ', scoresStdDev[8])
-print('svcLOO: ', scoresStdDev[9])
-print('Log: ', scoresStdDev[10])
-print('LogLOO: ', scoresStdDev[11])
-
-#print('RF: ', scoresAv[0])
-#print('RFLOO: ', scoresAv[1])
-#print('NB: ', scoresAv[2])
-#print('NBLOO: ', scoresAv[3])
-#print('DT: ', scoresAv[4])
-#print('DTLOO: ', scoresAv[5])
-#print('Lin: ', scoresAv[6])
-#print('LinLOO: ', scoresAv[7])
-#print('svc: ', scoresAv[8])
-#print('svcLOO: ', scoresAv[9])
-#print('Log: ', scoresAv[10])
-#print('LogLOO: ', scoresAv[11])
-
-
-end = time.time()
-print('total time: ', end-start)
-
-
 #*******************************************************************
 # For LOO scores using different sized collections, different models
 #*******************************************************************
@@ -248,49 +366,6 @@ print('LinSVC')
 end = time.time()
 print('total time: ', end-start)
 sys.exit()
-
-
-
-
-#***********************************************
-# For getting accuracy of combined collections
-#***********************************************
-
-combined = tester.combine_collections(['Brain Networks', 'Biological Networks'], 'Brain-Bio')
-combTesterBnBio = ModelTester(combined)
-
-combined = tester.combine_collections(['Web Graphs', 'Technological Networks'], 'Web-Tech')
-combTesterWbTch = ModelTester(combined)
-
-combined = combTesterBnBio.combine_collections(['Web Graphs', 'Technological Networks'], 'Web-Tech')
-combTesterBoth = ModelTester(combined)
-
-scoresArr = np.zeros(3)
-for i in range(100):
-    webTech = combTesterWbTch.modelFitTest(NBModel, dropList=remove, prnt=False, LOO=True)
-    brainBio = combTesterBnBio.modelFitTest(NBModel, dropList=remove, prnt=False, LOO=True)
-    both = combTesterBoth.modelFitTest(NBModel, dropList=remove, prnt=False, LOO=True)
-
-    currentScores = [ webTech,
-                      brainBio,
-                      both
-                      ]
-    arr = np.array(currentScores)
-    scoresArr += arr
-    print( 'Iteration ', i )
-
-
-scoresAv = scoresArr / 100
-print('WebTech: ', scoresAv[0])
-print('BrainBio: ', scoresAv[1])
-print('Both: ', scoresAv[2])
-
-sys.exit('Im done')
-
-
-
-
-
 
 
 
