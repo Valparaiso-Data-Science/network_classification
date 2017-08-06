7034ac9c30187e9acd32a37180472f23c33990d3
adriananaortiz <adriana.ortiz10@upr.edu>
2017-08-06 11:11:19 -0500

    Deleted unnecessary comments

--- src/visualization/Collections_pie_chart.py
+++ src/visualization/Collections_pie_chart.py
@@ -7,8 +7,8 @@ from bokeh.charts import Bar, output_file, show
 
 
 # Read file
-df = pd.read_csv('~/Downloads/network_classification/src/data/data_minmaxscale.csv', index_col=0)
-df_old = pd.read_csv('~/Downloads/network_classification/src/data/data_without_dimacs_or_bhoslib.csv', index_col=0)
+df = pd.read_csv('./../data/data_minmaxscale.csv', index_col=0)
+df_old = pd.read_csv('./../data/data_without_dimacs_or_bhoslib.csv', index_col=0)
 
 # Plot pie chart with Collection column of df
 #df['Collection'].value_counts().plot.pie(fontsize = 10.5, autopct='%1.0f%%')
@@ -17,23 +17,24 @@ df_old = pd.read_csv('~/Downloads/network_classification/src/data/data_without_d
 ## Show plot
 #plt.show()
 my_colors = 'rgbkymc'
-df_old['Collection'].value_counts().plot.bar(color=my_colors, fontsize=15)
-plt.title('Counts of Collections Before Downsampling', fontsize=25)
-#plt.show()
+p1 = plt.figure()
+myfig=df_old['Collection'].value_counts().plot.bar(color=my_colors, fontsize=15)
+
+#plt.title('Counts of Collections Before Downsampling', fontsize=25)
+myfig.set_axisbelow(True)
+myfig.yaxis.grid(color='gray', linestyle='dashed')
 
 
 ##plot bar chart with collection column of df
-df['Collection'].value_counts().plot.bar(color=my_colors, fontsize=15)
-plt.title('Counts of Collections After Downsampling', fontsize=25)
+p2 = plt.figure()
+myfig2 = df['Collection'].value_counts().plot.bar(color=my_colors, fontsize=15)
+#plt.title('Counts of Collections After Downsampling', fontsize=25)
+myfig2.set_axisbelow(True)
+myfig2.yaxis.grid(color='gray', linestyle='dashed')
+
+show(p1,p2)
+#myfig2.show()
 
 # Show plot
 #plt.show()
-
-# Plot in bokeh
-
-
-dict_old = ColumnDataSource(dict(x=df_old['Collection'].value_counts(), y = df_old['Collection'].unique(),))
-p=figure(title = 'Collections of graphs ', plot_width=1000)
-p.vbar(bottom=0, top=df['Collection'].value_counts(), x=df['Collection'].unique(), width=100)
-p.vbar(bottom=0, top=df_old['Collection'].value_counts(), x=df_old['Collection'].unique(), width=100)
 
